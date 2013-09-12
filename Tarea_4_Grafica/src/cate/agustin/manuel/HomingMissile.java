730e7c84b88196699ce47292f57372846b38c110
Manuel Olguin <molguin@dcc.uchile.cl>
2013-09-11 21:07:34 -0300

    Empezando construccion de HomingMissile

---
+++ Tarea_4_Grafica/src/cate/agustin/manuel/HomingMissile.java
@@ -0,0 +1,43 @@
+package cate.agustin.manuel;
+
+import org.mini2Dx.core.graphics.Graphics;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class HomingMissile implements SpaceObject{
+
+	public HomingMissile(){
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+  public boolean deleteThis() {
+	  // TODO Auto-generated method stub
+	  return false;
+  }
+
+	@Override
+  public void renderObject(Graphics g) {
+	  // TODO Auto-generated method stub
+	  
+  }
+
+	@Override
+  public void updatePosition(float delta) {
+	  // TODO Auto-generated method stub
+	  
+  }
+
+	@Override
+  public Vector2 getPosition() {
+	  // TODO Auto-generated method stub
+	  return null;
+  }
+
+	@Override
+  public Vector2 getRealPosition() {
+	  // TODO Auto-generated method stub
+	  return null;
+  }
+
+}
